09a8925e0a2bc5844dd46305fd5cd4c9a4e141d1
Elena Silchenko <elenasilchenko@Dmitrys-MacBook-Pro.local>
2024-07-12 17:11:18 +0200

    Add new component Scheduler

--- src/Main/index.js
+++ src/Main/index.js
@@ -1,9 +1,11 @@
 import Week from "./Week";
+import Scheduler from "./Week/Scheduler";
 
 export default function Main() {
     return (
         <div>
             <Week />
+            <Scheduler />
         </div>
     );
 }
